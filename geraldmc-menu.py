0c820272afc4b9a7b60860d984e511c488272071
geraldmc <gerald.mccollam@gmail.com>
2025-01-12 08:54:56 -0600

    geraldmc menu.py, third version

--- geraldmc-menu.py
+++ geraldmc-menu.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import argparse
 
 def menu(*args):
   while True:
-    choice = ' '.join(args) + "\n"
-    inp = input(f"Select one of: \n { choice }").strip()
-    if inp in args:
+    for arg in args:
+     inp = input(f"Select one of: {arg}")
+    if inp in arg:
       print(f'You entered {inp}. Bye!')
       return None
     else:
       print(f'{inp} not found.')
 
 if __name__ == '__main__':
-  menu('a','b','c')
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-s', '--strings', nargs='+', help='Entered strings, e.g. a, b, c')
+  args = parser.parse_args()
+  menu(args.strings)
