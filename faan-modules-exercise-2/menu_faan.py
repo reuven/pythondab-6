10115a408891d4c1c1cc666bfce4ad03c8fcb9d7
Faan DeSwardt <faan.deswardt@rubrik.com>
2025-01-15 06:37:04 -0700

    feat: Initial version of menu.py
    - Solution for Exercise 2 from Intro to Python Modules and Packages
    - Solution 1 of 3

---
+++ faan-modules-exercise-2/menu_faan.py
@@ -0,0 +1,31 @@
+# (1) Write a module, "menu.py", in which you define a single function,  "menu". This function should take any number of string arguments,  which are then presented to the user.
+#
+# If the user chooses one of the arguments (as a string), then that  string is returned from the function.  If not, then the user is  given another chance to enter a choice.
+#
+# For example:
+#
+#         import menu
+#         user_choice = menu.menu('a', 'b', 'c')
+#
+# (2) Modify your module, such that running it from the command line asks the user to enter 'a', 'b', or 'c' and then prints the result.
+#
+# (3) Now modify your module further, such that running it from the command line uses "sys.argv" to get any command-line
+# arguments as a list of strings.  Pass those strings to the "menu" function, and ask the user to choose from among them.
+# Print the user's choice.
+
+import sys
+
+
+def menu(*args):
+  while True:
+    user_selection = input(f"Select one of: {args} : ").strip()
+    if user_selection in args:
+      return user_selection
+    else:
+      print(f'{user_selection} not found.')
+      continue
+
+
+if __name__ == '__main__':
+  selection = menu('a', 'b', 'c')
+  print(f'You selected {selection}')
