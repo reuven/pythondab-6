900f9f8f291b34a652898453d3f92c4ce75c67cb
Ray Doue <rrdoue@gmail.com>
2025-01-16 16:27:00 -0600

    I liked the way Norman's version of this exercise accounted for all three scenarios in the exercise (Exercise 2, Section 2, Intro-Python-Modules-and-Packages), so spent a little more time to see whether I could find a way to package this up better.  Having had so much trouble guessing how to run other peoples' solutions, I also attempted to provide enough information so the average user can avoid trial and error.

--- rrdoue/rrdoue_menu.py
+++ rrdoue/rrdoue_menu.py
@@ -1,33 +1,40 @@
 #! /usr/bin/env python3
-"""The rrdoue_menu module, with one function, when called with any number of arguments, asks a user to choose from a list of the arguments and returns the user's choice. For example, rrdoue_menu.menu('1', '2', '3', '4', '5')
-When run from the command line, execute the script with any number of choices (not quoted) separated by spaces.  For example, python3 <script_name>.py a b c"""
+"""The rrdoue_menu module, with one function, when called with any number of arguments, asks a user to choose from the list of arguments and returns the user's choice. For example, "rrdoue_menu.menu('1', '2', '3', '4', '5')".
+When run from the command line, execute the script with any number of choices (not quoted) separated by spaces.  For example, "python3 rrdoue_menu.py a b c".  However, if executed from the command line without any parameters, the script executes a standard menu including a, b, and c.
+Brief command-line help is available by calling the script with -h or --help."""
 
+import sys
 
-from sys import argv
 
 def menu(*args):
     """
-    :param args: Any number of arguments in the form of a comma-separated single-quoted list.
+    :param args: Any number of string arguments in the form of a comma-separated single-quoted list.
     :return: The user's choice from the list of arguments.
     """
 
-    debug = False
+    options = '/'.join(args)  # Reuven used this in his solution
 
     while True:
 
-        user_response: str = input(f'Please enter your choice, one of {args}: ')
+        user_response: str = input(f'Please enter a choice, one of {options}: ')
 
         if user_response in args:
-            if debug:
-                print(f'User choice is {user_response}')
             return user_response
 
         print(f'"{user_response}" is not valid, please try again.')
 
 
 if __name__ == "__main__":
-    if argv[1] == '-h' or argv[1] == '--help':
+    if len(sys.argv) == 1:
+        print(f'Using default choices a, b, and c')
+        user_choice = menu('a', 'b', 'c')
+        print(f'You chose `{user_choice}`.')
+    elif (len(sys.argv) == 2
+          and sys.argv[1] == '-h'
+          or sys.argv[1] == '--help'):
         print('Execute the script from the command line with any number of choices (not quoted) separated by spaces.  '
-              'For example, python3 <script_name>.py a b c')
+              'For example, "python3 rrdoue_menu.py a b c"')
+        exit()
     else:
-        print(f"'{menu(*argv[1:])}'")
+        user_choice = menu(*sys.argv[1:])
+        print(f'You chose `{user_choice}`.')
