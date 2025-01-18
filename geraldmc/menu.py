cbea6fbc4c91533c6a4f283860ffd7fe727d0f37
geraldmc <gerald.mccollam@gmail.com>
2025-01-18 06:43:16 -0600

    adds menu, second try

--- geraldmc/menu.py
+++ geraldmc/menu.py
@@ -3,11 +3,13 @@
 
 def menu(*args):
   while True:
-    choices = ' '.join(args)
-    choice = choices + "\n"
+    choice = ' '.join(args) + "\n"
     inp = input(f"Select one of: \n { choice }").strip()
     if inp in args:
       print(f'You entered {inp}. Bye!')
       return None
     else:
       print(f'{inp} not found.')
+
+if __name__ == '__main__':
+  menu('a','b','c')
